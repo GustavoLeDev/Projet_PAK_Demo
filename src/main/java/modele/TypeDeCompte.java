706e7d49628f6ca27e5cf09f2b9c91c2301101f0
kekonaminga@gmail.com <kekonaminga@gmail.com>
2023-06-28 10:22:30 +0200

    vs

--- src/main/java/modele/TypeDeCompte.java
+++
@@ -1,9 +0,0 @@
-package modele;
-
-/**
- * Cette enumération représente les différents types de comptes de l'application.
- * Elle est notamment utilisée dans la classe "Utilisateur" pour l'attribut "typeDeCompte".
- */
-public enum TypeDeCompte {
-    UTILISATEUR, ADMINISTRATEUR, SUPER_ADMINISTRATEUR
-}
